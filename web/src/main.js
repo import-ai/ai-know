93d78545c3f9f2597f8675779b7a16057aea8147
Mike Zhao <yichenzhao@proton.me>
2024-07-07 19:07:23 +0800

    web: add tailwindcss

--- web/src/main.js
+++ web/src/main.js
@@ -1,9 +1,11 @@
-import { createApp } from 'vue'
-import Toast from "vue-toastification";
+import '@/assets/css/tailwind.css'
 import "vue-toastification/dist/index.css";
 
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue'
+import Toast from "vue-toastification";
+
+import App from '@/App.vue'
+import router from '@/router'
 
 const app = createApp(App)
 
