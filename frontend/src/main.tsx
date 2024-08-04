525f5dc0299479bf21b5e20666cb48f6c5fb74ac
seiry yu <4397354+seiry@users.noreply.github.com>
2024-08-04 20:00:48 +0800

    chore: Update TypeScript import module specifier preference

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Root from '@/routes/root'
-import { Article } from './components/Article'
+import { Article } from '@/components/Article'
 
 const router = createBrowserRouter([
   {
