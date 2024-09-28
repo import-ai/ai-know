3703a2394036de89e4aab30d34c1c8d3226e7ddf
Seiry Yu <4397354+seiry@users.noreply.github.com>
2024-09-28 12:28:05 +0800

    Update backend URL and include additional files in tsconfig

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,29 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { resolve } from 'path'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    //
-  ],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
+//@ts-ignore
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+
+  return defineConfig({
+    plugins: [
+      react(),
+      //
+    ],
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, './src'),
+      },
     },
-  },
-})
+    server: {
+      proxy: {
+        '/api': {
+          target: process.env.VITE_BACKEND_URL,
+          changeOrigin: true,
+          // rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
+    },
+  })
+}
