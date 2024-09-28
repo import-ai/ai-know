88604943d025316d9b459aa3ae3ac4446e625b4c
Seiry Yu <4397354+seiry@users.noreply.github.com>
2024-09-28 14:08:39 +0800

    Add clsx library for class name manipulation

--- frontend/src/components/Demo/testApi.tsx
+++ frontend/src/components/Demo/testApi.tsx
@@ -2,19 +2,57 @@ import {
   useGetApiSidebarEntriesEntryId,
   useGetApiSidebarEntriesEntryIdSubEntries,
   useGetApiWorkspace,
+  usePostApiSidebarEntries,
 } from '@/api'
 import { usePrivateEnties } from '@/hooks/workspace'
+import clsx from 'clsx'
+import dayjs from 'dayjs'
+import { useCallback } from 'react'
 
 export const TestApi = () => {
   const privateId = usePrivateEnties()
   const { data: sidebarData } = useGetApiSidebarEntriesEntryId(privateId ?? '')
-  const { data: subdata } = useGetApiSidebarEntriesEntryIdSubEntries(
-    privateId ?? '',
-  )
+  const { data: subData, mutate: refreshData } =
+    useGetApiSidebarEntriesEntryIdSubEntries(sidebarData?.data.entry?.id ?? '')
+  const { trigger: create, isMutating } = usePostApiSidebarEntries()
+  const createEntry = useCallback(() => {
+    create({
+      parent: sidebarData?.data.entry?.id,
+      type: 'note',
+      title: 'new sub ' + dayjs().format('YYYY-MM-DD HH:mm:ss'),
+    }).finally(() => {
+      refreshData()
+    })
+  }, [create, refreshData, sidebarData?.data.entry?.id])
+
   return (
-    <div>
-      {JSON.stringify(sidebarData?.data)}
-      <div>{JSON.stringify(subdata?.data)}</div>
+    <div className="relative">
+      <div
+        className={clsx(
+          'absolute h-full w-full flex items-center justify-center bg-slate-400 bg-opacity-85',
+          { hidden: !isMutating },
+        )}
+      >
+        <span className="loading loading-dots loading-xl"></span>
+      </div>
+
+      <code className="m-5">{JSON.stringify(sidebarData?.data)}</code>
+      <code className="m-5">{JSON.stringify(subData?.data)}</code>
+
+      <div className="m-3">
+        <h1>{sidebarData?.data.entry?.title}</h1>
+        {subData?.data.sub_entries?.map((sub) => (
+          <div key={sub.id} className="ml-2">
+            <h2>
+              {sub.title} /id:{sub.id}
+            </h2>
+          </div>
+        ))}
+      </div>
+
+      <button className="btn" onClick={createEntry} disabled={isMutating}>
+        add a sub
+      </button>
     </div>
   )
 }
